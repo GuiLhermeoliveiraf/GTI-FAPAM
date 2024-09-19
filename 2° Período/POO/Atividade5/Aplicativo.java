adcab7a97e045458287b0f3646a24d2a6a9a5fe1
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-09-19 17:32:38 -0300

    Update Aplicativo.java

--- 2° Período/POO/Atividade5/Aplicativo.java
+++ 2° Período/POO/Atividade5/Aplicativo.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Aplicativo {
+    // Main do aplicativo 
     public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in);
     
     System.out.println("Programa esta iniciando");
     Computador computador1 = new Computador();
     
+    // exibe as configurações do contrutor passadas pelo dev
     System.out.println(computador1.exi_info());
 
+    // Metado que atribuei novos valores a variavel 
     computador1.Atu_valores(null, 0, 0);
     
     System.out.println("Digite a sua marca: ");
@@ -20,8 +23,10 @@ public class Aplicativo {
     System.out.println("Digite sua quantidade de armazenamento: ");
     computador1.setArmazenamento(entrada.nextInt());
 
+    // Exibe as novas configurações do computador 
     System.out.println(computador1.exi_info());
 
+    // Metado que verifica se Computador é rapido ou lento
     System.out.println(computador1.veri_velo());
 
     }
