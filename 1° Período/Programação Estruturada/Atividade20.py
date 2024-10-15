d2fa131e60e658774b1e0c5c54ea4b0c118ce801
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-14 22:37:20 -0300

    Create Atividade20.py

---
+++ 1° Período/Programação Estruturada/Atividade20.py
@@ -0,0 +1,5 @@
+"//receba numero e mostre * 2 * 3 e raiz quadrada//"
+num = int(input("Digite um numero: "))
+print(f"\n {num} x 2 é igual a = {num * 2}")
+print(f"\n {num} x 3 é igual a = {num * 3}")
+print(f"\n A raiz quadrada de {num} é igual a = {num**(1/2):.2f}")
