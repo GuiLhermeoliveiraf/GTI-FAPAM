a2f9be729701cbb6ca080215b6ea15981663c410
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-14 22:36:42 -0300

    Create Atividade19.py

---
+++ 1° Período/Programação Estruturada/Atividade19.py
@@ -0,0 +1,4 @@
+"# recendo numero do usuario e mostrando antes e depois"
+num = int(input("Digite um numero: "))
+print(f"\n{num} + 1 é igual a = {num + 1}")
+print(f"\n{num} - 1 é igual a = {num - 1}")
