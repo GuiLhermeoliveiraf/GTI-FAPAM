a09e35de38fe2cb4d080b15282a6729d804c95c3
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-14 22:38:56 -0300

    Create Atividade24.py

---
+++ 1° Período/Programação Estruturada/Atividade24.py
@@ -0,0 +1,3 @@
+"# pega o valor de usuario e mostra os dolares"
+valor = float(input("Quanto você tem na sua carteira? R$: "))
+print(f"\nO valor de {valor:.2f} daria pra ter {(valor/3.27):.2f} dolares ")
