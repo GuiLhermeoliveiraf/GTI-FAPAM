27ab468fc2263937873b25a0d0542fab37ea9b10
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-14 22:37:47 -0300

    Create Atividade21.py

---
+++ 1° Período/Programação Estruturada/Atividade21.py
@@ -0,0 +1,4 @@
+"# Pegando a nota e fazendo a media"
+nota1 = float(input("Qual foi a sua primeira nota ?: "))
+nota2 = float(input("\nQual foi a sua segunda nota ?: "))
+print(f"\nA medía de {nota1} e {nota2} é igual a = {(nota1 + nota2)/2:.1f}")
