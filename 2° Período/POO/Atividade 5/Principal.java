b691fe929ed74baea2eb8946a6c88a3d6a05e11f
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-16 08:37:00 -0300

    Create Principal.java

---
+++ 2° Período/POO/Atividade 5/Principal.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args){
+    Scanner entrada = new Scanner(System.in);
+    
+    Livro liv1 = new Livro(null, null, 0, 1);
+
+    System.out.println("Digite o nome do livro");
+    liv1.setLivro(entrada.nextLine());    
+
+    System.out.println("Digite o nome do autor");
+    liv1.setAutor(entrada.nextLine());
+
+    System.out.println("Digite o ano da publicação: ");
+    liv1.setAnopublicacao(entrada.nextInt());
+
+    Biblioteca acervo = new Biblioteca("Fapam");
+    acervo.adicionarLivro(liv1);
+
+    acervo.exibirLivros();
+
+    System.out.println("Digite o ano em que deseja ver os livros");
+    acervo.exibirlivros(2024, liv1);
+
+    acervo.excluirlivros(liv1);
+
+
+
+
+
+
+
+
+
+
+    }
+}
