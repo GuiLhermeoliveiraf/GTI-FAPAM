ba91e5a743910f17bc6d6c2a556d5c909b7dee19
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-17 07:50:48 -0300

    Update Principal.java

--- 2° Período/POO/Atividade 5/Principal.java
+++ 2° Período/POO/Atividade 5/Principal.java
@@ -1,40 +1,44 @@
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args){
-    Scanner entrada = new Scanner(System.in);
-    
-    Livro liv1 = new Livro(null, null, 0, 1);
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-    System.out.println("Digite o nome do livro");
-    liv1.setLivro(entrada.nextLine());    
+        Livro liv1 = new Livro(null, null, 0, 1);
 
-    System.out.println("Digite o nome do autor");
-    liv1.setAutor(entrada.nextLine());
+        System.out.println("Digite o nome do livro");
+        liv1.setLivro(entrada.nextLine());
 
-    System.out.println("Digite o ano da publicação: ");
-    liv1.setAnopublicacao(entrada.nextInt());
+        System.out.println("Digite o nome do autor");
+        liv1.setAutor(entrada.nextLine());
 
-    Biblioteca acervo = new Biblioteca("Fapam");
-    acervo.adicionarLivro(liv1);
+        System.out.println("Digite o ano da publicação: ");
+        liv1.setAnopublicacao(entrada.nextInt());
+        entrada.nextLine();
 
-    liv1.status();
+        Biblioteca acervo = new Biblioteca("Fapam");
+        acervo.adicionarLivro(liv1);
 
-    acervo.exibirLivros();
+        liv1.status();
 
-    System.out.println("Digite o ano em que deseja ver os livros");
-    acervo.exibirlivros(2024);
+        System.out.println();
 
-    acervo.excluirlivros("Teste");
+        acervo.exibirLivros();
 
+        System.out.println();
 
+        System.out.println("Digite o ano em que deseja ver os livros");
+        acervo.exibirlivros(entrada.nextInt());
+        entrada.nextLine();
 
+        System.out.println();
 
+        System.out.println("Digite o nome do livro que deseja excluir:");
+        acervo.excluirlivros(entrada.nextLine());
+        entrada.nextLine();
 
-
-
-
-
+        System.out.println("Estamos encerrando nosso expedinte, obrigado por vir");
 
     }
+}
 }
