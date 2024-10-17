75a96e7295e6d55263392cfee92387a01e618044
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-17 16:40:58 -0300

    Update Biblioteca.java

--- 2° Período/POO/Atividade 5/Biblioteca.java
+++ 2° Período/POO/Atividade 5/Biblioteca.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+
 public class Biblioteca {
+
     private String nome;
     ArrayList<Livro> livros;
-       
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -16,42 +18,32 @@ public class Biblioteca {
         this.livros = new ArrayList<Livro>();
     }
 
-    public void adicionarLivro(Livro livro){
+    public void adicionarLivro(Livro livro) {
         livros.add(livro);
     }
 
-    public void exibirLivros(){
-        for(Livro livro : livros){
-            System.out.println("Acevo " + livro.getLivro());
+    public void exibirLivros() {
+        for (Livro livro : livros) {
+            System.out.println("Acevo: " + livro.getLivro());
+        }
+        System.out.println("Fim do acervo");
+    }
+
+    public void exibirlivros(int ano) {
+        for (Livro livro : livros) {
+            if (ano == livro.getAnopublicacao()) {
+                System.out.println("Nosso acervo: " + livro.getLivro());
+            }
         }
     }
 
-    public void exibirlivros(int ano){
-      for(Livro livro : livros ){
-        if(ano == livro.getAnopublicacao()){
-            System.out.println(getNome());
+    public void excluirlivros(String titulo) {
+        for (Livro livro : livros) {
+            if (titulo.equals(livro.getLivro())) {
+                System.out.println(livro.getLivro() + "Livro removido com sucesso ");
+            }
         }
-      }
+        livros.removeIf(livro -> titulo.equals(livro.getLivro()));
     }
-
-    public void excluirlivros(String titulo){
-       for(Livro livro : livros){
-        if(titulo == livro.getLivro()){
-            livros.remove(livro);
-        }
-       } 
-    }
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
