b01d25b646562e986e0f39dd744499e0491029ab
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-17 07:49:16 -0300

    Update Livro.java

--- 2° Período/POO/Atividade 5/Livro.java
+++ 2° Período/POO/Atividade 5/Livro.java
@@ -1,10 +1,9 @@
-public class Livro{
+public class Livro {
     private String livro;
     private String autor;
     private int anopublicacao;
     final int codigolivro;
-    
-    
+
     public Livro(String livro, String autor, int anopublicacao, int codigolivro) {
         setLivro(livro);
         setAutor(autor);
@@ -12,55 +11,47 @@ public class Livro{
         this.codigolivro = codigolivro;
     }
 
-
-    public Livro(String livro, String autor, int anopublicacao) {   
+    public Livro(String livro, String autor, int anopublicacao) {
         this.codigolivro = 0;
         setLivro(livro);
         setAutor(autor);
         this.anopublicacao = 2024;
     }
 
-
     public String getLivro() {
         return livro;
     }
-
 
     public String getAutor() {
         return autor;
     }
 
-
     public int getAnopublicacao() {
         return anopublicacao;
     }
-
 
     public int getCodigolivro() {
         return codigolivro;
     }
 
-
     public void setLivro(String livro) {
         this.livro = livro;
     }
-
 
     public void setAutor(String autor) {
         this.autor = autor;
     }
 
-
     public void setAnopublicacao(int anopublicacao) {
         this.anopublicacao = anopublicacao;
     }
 
-    public void exibirdetalhes(){
+    public void exibirdetalhes() {
         System.out.println("Apresentando o autor " + getAutor());
         System.out.println("Publicou o livro " + getLivro());
     }
-    
-    public void status(){
+
+    public void status() {
         System.out.println("Apresentando o autor " + getAutor());
         System.out.println("Publicou o livro " + getLivro());
         System.out.println("Publicado no ano de " + getAnopublicacao());
