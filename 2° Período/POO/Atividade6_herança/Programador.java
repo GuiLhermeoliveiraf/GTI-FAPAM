00d3d76d094492f53e4e0eabccd92390df0b19ce
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-24 23:37:49 -0300

    Update Programador.java

--- 2° Período/POO/Atividade6_herança/Programador.java
+++ 2° Período/POO/Atividade6_herança/Programador.java
@@ -1,42 +1,29 @@
-public class Programador extends Funcionario{
+public class Programador extends Funcionario {
     public String linguagem;
 
-    public Programador(String nome, float salario, String linguagem){
+    // Contrutor Sobrescrevendo, o metado da Classe Funcionario e adicionando o atributo linguagem da classe Programador
+    public Programador(String nome, float salario, String linguagem) {
         super(nome, salario);
         setLinguagem(linguagem);
     }
 
-    public void setLinguagem(String linguagem){
+    // Metados Getters e Setters
+    public void setLinguagem(String linguagem) {
         this.linguagem = linguagem;
     }
-    
-    public String getLinguagem(){
+
+    public String getLinguagem() {
         return linguagem;
     }
-    
-    
 
-    public String exibirDados(){
+    // Metado exibir dados sobrescrito da classe Funcionario, adicionando a linguagem
+    public String exibirDados() {
         String retorno = "";
 
         retorno = super.exibirDados() + " Linguagem " + getLinguagem();
-        
+
         return retorno;
-    
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
