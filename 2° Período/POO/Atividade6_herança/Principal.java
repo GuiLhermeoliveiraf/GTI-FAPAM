4b6c759d185bbc1fe55654eb040f737f4766880d
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-10-24 23:37:29 -0300

    Update Principal.java

--- 2° Período/POO/Atividade6_herança/Principal.java
+++ 2° Período/POO/Atividade6_herança/Principal.java
@@ -1,14 +1,14 @@
-public class Principal{
-    public static void main(String [] args){
+public class Principal {
+    public static void main(String[] args) {
+        // Criando objetos das classes Funcinario, Gerente e Progamador
         Funcionario fun1 = new Funcionario("Guilherme", 1500f);
         Gerente ger1 = new Gerente("Gabriel", 1600f, 300f);
         Programador pro1 = new Programador("Lucas", 2000f, "java");
 
+        // Mostrando os metados exibir dados de cada das classes 
         System.out.println(fun1.exibirDados());
         System.out.println(ger1.exibirDados());
         System.out.println(pro1.exibirDados());
-        
-        
 
     }
 }
