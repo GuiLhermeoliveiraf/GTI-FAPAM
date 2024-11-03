e3df6df246cfb5ef65d2532f885fbfc273b63008
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-02 21:43:29 -0300

    Update Caminhao.java

--- 2° Período/POO/Atividade7_Polimofismo/Caminhao.java
+++ 2° Período/POO/Atividade7_Polimofismo/Caminhao.java
@@ -1,11 +1,14 @@
+// Criando a classe Caminhao que herda valores da classe Veiculo
 public class Caminhao extends Veiculo{
     private int capacidadeCarga;
 
+    // Metado contrutor da Classe Caminhao
     public Caminhao(String modelo, int ano, int capacidadeTotal, int capacidadeCarga) {
         super(modelo, ano, capacidadeTotal);
         this.capacidadeCarga = capacidadeCarga;
     }
 
+    // Metado detalhes sobreescrito da classe Veiculo que adiciona a Capacidade de Carga 
     @Override
     public String detalhes() {
         return super.detalhes() + ". Capidade de Carga: " + capacidadeCarga;
