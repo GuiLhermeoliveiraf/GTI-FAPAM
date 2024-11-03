da2a69f56469c4a0be92eb6b6f0e19fbf26c5b66
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-02 21:44:08 -0300

    Update Frota.java

--- 2° Período/POO/Atividade7_Polimofismo/Frota.java
+++ 2° Período/POO/Atividade7_Polimofismo/Frota.java
@@ -1,27 +1,31 @@
+// Criando a classe Frota
 public class Frota {
     private String nome;
     private Veiculo[] veiculos;
     private int calculadora;
 
+    // Metado Contrutor da classe Frota
     public Frota(String nome) {
         this.nome = nome;
         veiculos = new Veiculo[10];
         this.calculadora = 0;
     }
 
+    // Metado Getter do atributo nome
     public String getNome(){
         return nome;
     }
 
+    // Metado que adiciona um valor do tipo Veiculo no Array
     public void adcionarVeiculo(Veiculo veiculo) {
         if (calculadora <= veiculos.length) {
             veiculos[calculadora] = veiculo;
             calculadora++;
-            System.out.println("Deu bom");
         }
 
     }
 
+    // Metado que mostra o metado detalhes de todos os Veiculos dentro de Array
     public String exibirVeiculos() {
         String retorno = "";
         for (int i = 0; i < veiculos.length; i++) {
@@ -35,3 +39,4 @@ public class Frota {
     }
 
 }
+
