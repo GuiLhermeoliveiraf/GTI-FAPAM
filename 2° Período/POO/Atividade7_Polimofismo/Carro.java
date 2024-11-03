5bd490534d625473decb204cf343e67bad25de8d
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-02 21:43:03 -0300

    Update Carro.java

--- 2° Período/POO/Atividade7_Polimofismo/Carro.java
+++ 2° Período/POO/Atividade7_Polimofismo/Carro.java
@@ -1,14 +1,17 @@
+// Classe Carro que herda valores de Veiculo
 public class Carro extends Veiculo {
     private Motor motor;
 
+    // Metados construtor que pega valores de Veiculo e adiconar um motor 
     public Carro(String modelo, int ano, int capacidadeTotal, Motor motor) {
         super(modelo, ano, capacidadeTotal);
         this.motor = motor;
     }
 
+    // Metado detalhes sobreescrito da classe Veiculo que adiciona o motor 
     @Override
     public String detalhes() {
-        return super.detalhes() + " Motor: " + this.motor.getTipo();
+        return super.detalhes() + ". Motor: " + this.motor.getTipo();
     }
 
 }
