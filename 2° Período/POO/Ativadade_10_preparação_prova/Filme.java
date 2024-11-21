087463c6c81cc508f629593e5518683cbbe201d4
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-20 22:11:35 -0300

    Update Filme.java

--- 2° Período/POO/Ativadade_10_preparação_prova/Filme.java
+++ 2° Período/POO/Ativadade_10_preparação_prova/Filme.java
@@ -1,17 +1,20 @@
+// Criando a classe Filme que herda da classe Midia 
 public class Filme extends Midia {
     private String diretor;
     private int duracao;
 
+    // Metado Contrutor
     public Filme(String titulo, int anoLancamento, String categoria, String diretor, int duracao) {
         super(titulo, anoLancamento, categoria);
         this.diretor = diretor;
         this.duracao = duracao;
     }
 
+    // Metados SobreEscritos da Classe Midia
     @Override
     public String exibirinfo() {
         return "Titulo : " + getTitulo() + " Ano Lancamento: " + getAnoLancamento() + " Diretor: " + getDiretor()
-                + " Duracao: " + getDuracao() + "Min" ;
+                + " Duracao: " + getDuracao() + "Min";
     }
 
     @Override
@@ -19,6 +22,7 @@ public class Filme extends Midia {
         return getCategoria();
     }
 
+    // Metados Getters
     public String getDiretor() {
         return diretor;
     }
