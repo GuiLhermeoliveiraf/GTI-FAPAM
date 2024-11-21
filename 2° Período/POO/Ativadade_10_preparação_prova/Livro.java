ac76a4ac56fe20d31ecfaadb6e93138a35367ae7
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-20 22:10:43 -0300

    Update Livro.java

--- 2° Período/POO/Ativadade_10_preparação_prova/Livro.java
+++ 2° Período/POO/Ativadade_10_preparação_prova/Livro.java
@@ -1,34 +1,41 @@
 import java.util.ArrayList;
 
+// Criando a classe Livro que herda da Midia
 public class Livro extends Midia {
     private int nuemroPaginas;
     private ArrayList<Autor> autores;
 
-    public Livro(String titulo, int anoLancamento, String categoria, int nuemroPaginas ) {
+    // Contrutor que recebe atributos da classe Pai
+    public Livro(String titulo, int anoLancamento, String categoria, int nuemroPaginas) {
         super(titulo, anoLancamento, categoria);
         this.nuemroPaginas = nuemroPaginas;
         this.autores = new ArrayList<>();
     }
 
+    // Metados Sobreescritos, adicionando informações
     @Override
     public String exibirinfo() {
         return "Titulo : " + getTitulo() + " Ano Lancamento: " + getAnoLancamento() + " Numero de Paginas: "
                 + getNuemroPaginas() + " Autores: " + listarAutores();
     }
 
+    // Metado que retorna o tipo de midia
     @Override
     public String tipoDeMidia() {
         return getCategoria();
     }
 
+    // Metado Getter
     public int getNuemroPaginas() {
         return nuemroPaginas;
     }
 
+    // Metado final que exibi as informações do livro
     public final String exibirResumo() {
         return getTitulo() + getNuemroPaginas();
     }
 
+    // Metado que valida a quantidade de paginas do livro
     public boolean validarLivro(int numeroMinimo) {
         if (nuemroPaginas > numeroMinimo)
             return true;
@@ -36,10 +43,12 @@ public class Livro extends Midia {
             return false;
     }
 
+    // Metado de gabiarra para adicionar um autor ao livro
     public void adicionarAutor(Autor autor) {
         autores.add(autor);
     }
 
+    // Metado que retorna todos os autores
     public String listarAutores() {
         String retorno = "";
 
