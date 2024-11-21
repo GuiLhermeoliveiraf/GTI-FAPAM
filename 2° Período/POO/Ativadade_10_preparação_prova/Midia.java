915201695e5815fdbb37e4af6c790eaeb9949f81
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-20 22:12:52 -0300

    Update Midia.java

--- 2° Período/POO/Ativadade_10_preparação_prova/Midia.java
+++ 2° Período/POO/Ativadade_10_preparação_prova/Midia.java
@@ -1,1 +1,33 @@
+// Craindo a classe Abstrata Midia
+public abstract class Midia {
+    private String titulo;
+    private int anoLancamento;
+    private String categoria;
+
+    // Metado Contrutor que sera herdado pelas subclasses
+    public Midia(String titulo, int anoLancamento, String categoria) {
+        this.titulo = titulo;
+        this.anoLancamento = anoLancamento;
+        this.categoria = categoria;
+    }
+
+    // Metados Getters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    // Metedos Abstratos que seram herdados pelas subclasses
+    public abstract String exibirinfo();
+
+    public abstract String tipoDeMidia();
+
+}
 
