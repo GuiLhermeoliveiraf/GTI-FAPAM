486e7769a41e802e7c9466bce8285f8a7db13075
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-20 22:10:25 -0300

    Update Autor.java

--- 2° Período/POO/Ativadade_10_preparação_prova/Autor.java
+++ 2° Período/POO/Ativadade_10_preparação_prova/Autor.java
@@ -1,12 +1,15 @@
+// Criando a Classe Autor
 public class Autor {
     private String nome;
     private int anoNascimento;
 
+    // Metado Contrutor
     public Autor(String nome, int anoNascimento) {
         this.nome = nome;
         this.anoNascimento = anoNascimento;
     }
 
+    // Metados Getters
     public String getNome() {
         return nome;
     }
@@ -15,10 +18,12 @@ public class Autor {
         return anoNascimento;
     }
 
+    // Metado exibir informações do autor 
     public String exibirAutor() {
         return "Nome : " + getNome() + ". Ano Nascimento : " + getAnoNascimento();
     }
 
+    // Metado que calcula a idade do autor 
     public int calcularIdade(int anoAtual) {
         int idade = 0;
 
@@ -28,5 +33,7 @@ public class Autor {
         return idade;
 
     }
+
+}
 
 }
