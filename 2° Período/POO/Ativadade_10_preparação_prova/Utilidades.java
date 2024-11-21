8d5fbf1ae7b2b59a664c3c3a383af1ed852ea53d
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-20 22:12:25 -0300

    Update Utilidades.java

--- 2° Período/POO/Ativadade_10_preparação_prova/Utilidades.java
+++ 2° Período/POO/Ativadade_10_preparação_prova/Utilidades.java
@@ -1,4 +1,6 @@
+// Crinado classe Utilidade
 public class Utilidade {
+    // Criando metado que retorna o Titulo de cada Midia dentro do array
     public static String exibirPorCategoria(Midia[] midias, String categoria) {
         String retorno = "";
         for (int i = 0; i < midias.length; i++) {
