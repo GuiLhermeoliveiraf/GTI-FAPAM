ff10dd4e4a53306caa2bee891223d0221c11125a
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-21 23:03:01 -0300

    Update Funcionario.java

--- 2° Período/POO/Trabalho_sala/Funcionario.java
+++ 2° Período/POO/Trabalho_sala/Funcionario.java
@@ -1,31 +1,44 @@
-public class Funcionario extends Pessoa{
+// Criando a classe Funcionario que herda de Pessoa 
+public class Funcionario extends Pessoa {
     private String cargo;
     private float salario;
 
-    public Professor(String nome, int idade, String genero, String cargo, float salario){
+    // Metados contrutor
+    public Funcionario(String nome, int idade, String genero, String cargo, float salario) {
         super(nome, idade, genero);
         this.cargo = cargo;
         this.salario = salario;
     }
 
-    public String falar(){
+    // Metados abstratos da classe Pessoa
+    public String falar() {
         return "Não quero falar nada";
     }
 
-    public String estudar(){
+    public String estudar() {
         return "Não estudei";
     }
 
+    // Metado Getters e Setters
+    public String getCargo() {
+        return cargo;
+    }
 
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
 
+    public float getSalario() {
+        return salario;
+    }
 
+    private void setSalario(float salario) {
+        this.salario = salario;
+    }
 
-
-
-
-
-
-
-
+    // Metado Aumentar salario
+    public void aumentarSalario(float salario) {
+        setSalario(getSalario() + salario);
+    }
 
 }
