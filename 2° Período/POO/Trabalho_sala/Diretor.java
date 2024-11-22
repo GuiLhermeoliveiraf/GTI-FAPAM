a0068c09eda1f3fb7a61b7291029de2b6925162e
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-21 23:02:40 -0300

    Update Diretor.java

--- 2° Período/POO/Trabalho_sala/Diretor.java
+++ 2° Período/POO/Trabalho_sala/Diretor.java
@@ -1,18 +1,40 @@
-public final class Diretor extends Professor{
+// Criando metado final Diretor que herda de Professor
+public final class Diretor extends Professor {
     private Escola escola;
     private int anoCargo;
 
-    public Diretor(String nome, int idade, String genero, int anoCargo, Escola escola){
-        super(nome, idade, genero);
+    // Metado Contrutor
+    public Diretor(String nome, int idade, String genero, float salario, Escola escola, int anoCargo) {
+        super(nome, idade, genero, salario);
         this.anoCargo = anoCargo;
         this.escola = escola;
     }
 
-    public String falar(){
+    // Metados abstratos da classe Pessoa
+    public String falar() {
         return "A mensalidade vai aumentar";
     }
-    
-    public String estudar(){
+
+    public String estudar() {
         return "Ja tenho doutorado";
     }
+
+    // Metados Getters
+    public Escola getEscola() {
+        return escola;
+    }
+
+    public int getAnoCargo() {
+        return anoCargo;
+    }
+
+    // Metados Setters
+    public void mudarEscola(Escola escola) {
+        this.escola = escola;
+    }
+
+    public void setAnoCargo(int anoCargo) {
+        this.anoCargo = anoCargo;
+    }
+
 }
