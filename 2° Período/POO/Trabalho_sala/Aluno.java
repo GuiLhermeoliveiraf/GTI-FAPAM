cac204e707a00734995d89e70f49468ea31b653c
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-21 23:01:55 -0300

    Update Aluno.java

--- 2° Período/POO/Trabalho_sala/Aluno.java
+++ 2° Período/POO/Trabalho_sala/Aluno.java
@@ -1,36 +1,40 @@
-public class Aluno extends Pessoa{
+// Criando classe Alunos que herda da classe Pessoa
+public class Aluno extends Pessoa {
     private final String ra;
     private Escola escola;
 
-    public Aluno(String nome, int idade, String genero, String ra, Escola escola){
+    // Metado Contrutor
+    public Aluno(String nome, int idade, String genero, String ra, Escola escola) {
         super(nome, idade, genero);
         this.ra = ra;
         this.escola = escola;
     }
 
-    public String falar(){
+    // Metados Sobreescritos da classe Pessoa
+    public String falar() {
         return "Me passa de ano por 50 no pix?";
     }
-    
-    public String estudar(){
+
+    public String estudar() {
         return "Ja estou estudando muito";
     }
 
-    
+    // Metados com sobrecarga
+    public String estudar(Materia materia) {
+        return "Estou estudando a materia: " + materia.getNome();
+    }
 
+    public String estudar(Professor professor) {
+        return "Estou estudando com o professor: " + professor.getNome();
+    }
 
+    // Metados Getters
+    public String getRa() {
+        return ra;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    public Escola getEscola() {
+        return escola;
+    }
 
 }
