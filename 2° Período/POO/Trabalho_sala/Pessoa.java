64309112f862cce68dd01a31345e155ec0c6babc
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-21 23:03:25 -0300

    Update Pessoa.java

--- 2° Período/POO/Trabalho_sala/Pessoa.java
+++ 2° Período/POO/Trabalho_sala/Pessoa.java
@@ -1,35 +1,38 @@
 // Classe abstrata Pessoa
-public abstract class Pessoa{
+public abstract class Pessoa {
     private String nome;
     private int idade;
-    private String genero
+    private String genero;
 
     // Contrutor
-    public Pessoa(String nome, int idade, String genero){
+    public Pessoa(String nome, int idade, String genero) {
         setNome(nome);
         setIdade(idade);
         setGenero(genero);
     }
 
     // Metados getters e Setters
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public int getIdade(){
+    public int getIdade() {
         return idade;
     }
-    public void setIdade(int idade){
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 
-    public int getGenero(){
+    public String getGenero() {
         return genero;
     }
-    public void setGenero(String genero){
+
+    public void setGenero(String genero) {
         this.genero = genero;
     }
 
@@ -38,8 +41,5 @@ public abstract class Pessoa{
     public abstract String falar();
 
     public abstract String estudar();
-    
-
-
 
 }
