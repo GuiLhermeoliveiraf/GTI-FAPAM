23f2bf576cbadda2b0aba015e4da509228b92560
Guilherme Oliveira <gguimaraes647@gmail.com>
2024-11-21 23:03:11 -0300

    Update Materia.java

--- 2° Período/POO/Trabalho_sala/Materia.java
+++ 2° Período/POO/Trabalho_sala/Materia.java
@@ -1,21 +1,37 @@
-public class Materia extends Escola{
+// Criando classe Materia que herda de Escola
+public class Materia extends Escola {
     private Professor professor;
-    private Alunos[] alunos;
+    private Pessoa[] alunos;
     private int contador;
     private String nome;
 
-    public Materia (Professor professor, String nome){
+    // Metados contrutor
+    public Materia(Professor professor, String nome) {
         this.nome = nome;
         this.professor = professor;
-        alunos = new Alunos[10];
+        alunos = new Pessoa[10];
         contador = 0;
     }
 
-    
+    // Metado Getter
+    public String getNome() {
+        return nome;
+    }
 
+    // Metados que adiciona aluno ao array se ele for uma instancia de Aluno o Bolsista
+    public void adicionarAluno(Pessoa aluno) {
+        if (contador < alunos.length) {
+            if (aluno instanceof Aluno || aluno instanceof Bolsista) {
+                alunos[contador] = aluno;
+                contador++;
+                System.out.println("Deu bom");
+            }
+        }
+    }
 
-
-
-
+    // Metado que retorna o nome do professor que leciona a Materia
+    public String nomeProfessor() {
+        return "O professor que leciona essa materia é o professor " + professor.getNome();
+    }
 
 }
